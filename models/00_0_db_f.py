7278ebc21cdf98d11a296bef9a60e51fb76c5489
Fernando Pardo <firedragon_fernando@hotmail.com>
2015-06-17 17:32:01 -0500

    preparación para el merge de ramas
    Cambios menores

--- models/00_0_db_f.py
+++ models/00_0_db_f.py
@@ -12,7 +12,8 @@
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
     # La base de datos debe existir
-    db = DAL('postgres://fpardo:Kamatar1@localhost/postgres',db_codec='UTF-8',check_reserved=['all'])
+    db = DAL('postgres://fpardo:Kamatar1@localhost/snmb_fusion_1', 
+        db_codec='UTF-8',check_reserved=['all'], migrate = False)
     #db = DAL('sqlite://storage.db',check_reserved=['all'])
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
